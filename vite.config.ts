7b0822253722d178d7388ba129bcab091f4d0163
KAJJAM YOGEENDRA <142445480+klu2200031399@users.noreply.github.com>
2025-04-29 09:38:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my_portifolio/',  // 👈 ADD THIS LINE: your GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
